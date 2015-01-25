37522d0aaf6f1aa60a645eccbedcaaeb0c5b26b2
palmerAL <PalmerAL@users.noreply.github.com>
2015-01-25 16:11:02 -0600

    cleanup

---
+++ extend-images.js
@@ -0,0 +1,21 @@
+//image insertion used in menu.js
+
+function imagedialogopen() {
+	        var input = "<img class='imageplaceholder'/>"; //add a placeholder to mark the cursor position
+        pasteHtmlAtCaret(input, false);
+$(".image-url-chooser").val(""); //reset the value
+$(".image-dialog").fadeIn(100);
+}
+
+function imagedialogclose() {
+	$(".image-dialog").fadeOut(100);
+	$(".imageplaceholder").remove();
+}
+
+function insertimagefromdialog() {
+	var newimage = $(".image-url-chooser").val();
+	var placeholder = $(".imageplaceholder");
+	placeholder.attr("src", newimage);
+	placeholder.removeClass("imageplaceholder").addClass("img-extend-block"); //use the placeholder to add an image
+	$(".image-dialog").fadeOut(100);
+}
--- menu.js
+++ menu.js
@@ -27,29 +27,12 @@ function insertsubheading() {
         pasteHtmlAtCaret(input, false);
 }
 
-function imagedialogopen() {
-	        var input = "<img class='imageplaceholder'/>"; //add a placeholder to mark the cursor position
-        pasteHtmlAtCaret(input, false);
-$(".image-url-chooser").val(""); //reset the value
-$(".image-dialog").fadeIn(100);
-}
-
-function imagedialogclose() {
-	$(".image-dialog").fadeOut(100);
-	$(".imageplaceholder").remove();
-}
-
-function insertimagefromdialog() {
-	var newimage = $(".image-url-chooser").val();
-	var placeholder = $(".imageplaceholder");
-	placeholder.attr("src", newimage);
-	placeholder.removeClass("imageplaceholder").addClass("img-extend-block"); //use the placeholder to add an image
-	$(".image-dialog").fadeOut(100);
-}
 //bind button events - use mousedown so that the buttons can't steal the focus
 
 $("#heading-insert").mousedown(insertheading);
 $("#subheading-insert").mousedown(insertsubheading);
+
+//these are defined in extend-images.js
 $("#image-insert").mousedown(imagedialogopen);
 $("#imagechooser-okay-button").click(insertimagefromdialog); //this doesn't need focus, you can use a regular click event
 $("#imagechooser-cancel-button").click(imagedialogclose); //this doesn't need focus, you can use a regular click event
