e275d02ae59e5743c510da726b24e14d77fc972a
palmerAL <PalmerAL@users.noreply.github.com>
2015-01-25 17:48:03 -0600

    add attribution to maps

--- extend-maps/maps.js
+++ extend-maps/maps.js
@@ -8,7 +8,8 @@ var long = coords[1]
 var map = L.map('map-main').setView([lat, long], 13);
 			L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
     maxZoom: 18,
-    subdomains: '1234'
+    subdomains: '1234',
+	attribution: "&copy; 2015 MapQuest"
 }).addTo(map);		
 
 //update the data fields
