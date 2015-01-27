2afade5560e5522c1342ec122c6a88c86dd91fb4
palmerAL <PalmerAL@users.noreply.github.com>
2015-01-27 17:07:33 -0600

    find in page: show # of matches

--- js/findinpage.js
+++ js/findinpage.js
@@ -9,24 +9,30 @@ TODO:
 
 */
 
+var findinpagecontainer = $(".findinpage");
 var findinpageinput = $("#findinpage-input");
 var editor = $("#document-editor");
+var findinpagecount = $(".findinpage-count");
 
 function startsearch() {
-findinpageinput.show().css("width", "0px").animate({width: "200px"}, 70).focus();
+findinpagecount.html("");
+findinpagecontainer.show().css("width", "0px").animate({width: "200px"}, 70);
+findinpageinput.focus();
 }
 
 function endsearch() {
-	findinpageinput.hide().val("");
+	findinpagecontainer.hide().css("width", "0px");
+	findinpageinput.val("");
 	editor.removeHighlight();
 }
 
 function highlightmatches() {
 	editor.removeHighlight();
 	editor.highlight(findinpageinput.val());
+	findinpagecount.html($(".highlight").length);
 }
 
-findinpageinput.hide();
+findinpagecontainer.hide();
 
 $("#findinpage-button").click(startsearch);
 
