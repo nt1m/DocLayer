a12d3ac6313f784a7b3a0695aa74d017e24e5bb7
palmerAL <PalmerAL@users.noreply.github.com>
2015-02-05 17:48:17 -0600

    use execCommand
    adds undo support

--- js/caret.js
+++ js/caret.js
@@ -4,33 +4,7 @@ scratchpad.modules.define("caret", {
 	pasteHtmlAtCaret: function(html, selectPastedContent) {
 		var sel, range;
 		if(document.activeElement.id == "document-editor") {
-			if (window.getSelection) {
-				// IE9 and non-IE
-				sel = window.getSelection();
-				if (sel.getRangeAt && sel.rangeCount) {
-					range = sel.getRangeAt(0);
-					range.deleteContents();
-
-					var el = document.createElement("div");
-					el.innerHTML = html;
-					var frag = document.createDocumentFragment(), node, lastNode;
-					while (node = el.firstChild) {
-						lastNode = frag.appendChild(node);
-					}
-					var firstNode = frag.firstChild;
-					range.insertNode(frag);
-				}
-			}
-			else if ((sel = document.selection) && sel.type != "Control") {
-				var originalRange = sel.createRange();
-				originalRange.collapse(true);
-				sel.createRange().pasteHTML(html);
-				if (selectPastedContent) {
-					range = sel.createRange();
-					range.setEndPoint("StartToStart", originalRange);
-					range.select();
-				}
-			}
+			document.execCommand("insertHTML", false, html); //TODO add fallback if this is unsupported by using the old caret api
 		} // end focus check
 		else {
 			$("#document-editor").append(html);
