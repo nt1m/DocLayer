00540cf1b12e222237b5241f44f1266264fe1f68
palmerAL <PalmerAL@users.noreply.github.com>
2015-02-12 16:44:28 -0600

    update research pane

--- js/findinpage.js
+++ js/findinpage.js
@@ -3,7 +3,6 @@
 /*
 
 TODO:
--show # matches
 -fuzzy search?
 -replace/replace all
 
@@ -16,6 +15,7 @@ scratchpad.modules.define("findinpage", {
 
 	startsearch: function() {
 		this.findinpagecount.html("");
+		this.findinpageinput.val("");
 		this.findinpagecontainer.show().css("width", "0px").animate({width: "200px"}, 70);
 		this.findinpageinput.focus();
 		if(window.location.hash != "#debug") {
@@ -25,7 +25,6 @@ scratchpad.modules.define("findinpage", {
 
 	endsearch: function() {
 		this.findinpagecontainer.hide().css("width", "0px");
-		this.findinpageinput.val("");
 		this.editor.removeHighlight();
 		this.editor.attr("contenteditable", "true"); //temp fix for firefox bug
 	},
@@ -35,35 +34,17 @@ scratchpad.modules.define("findinpage", {
 		this.editor.highlight(this.findinpageinput.val());
 		this.findinpagecount.html($(".highlight").length);
 	},
-	
-	showinfo: function() {
-		var search = this.findinpageinput.val();
-		var cardbox = $(".infocard-shell");
-		cardbox.html("");
-						infocard = new InfoCard({
-						query: search,
-						container: cardbox[0]
-				});
-	},
-	
-	hideinfo: function() {
-		$(".infocard-shell").html("");
-	},
 
 	init: function() {
 		// Bind functions
 		this.startsearch = this.startsearch.bind(this);
 		this.endsearch = this.endsearch.bind(this);
 		this.highlightmatches = this.highlightmatches.bind(this);
-		this.showinfo = this.showinfo.bind(this);
-		this.hideinfo = this.hideinfo.bind(this);
 
 		// Event listeners
 		this.findinpagecontainer.hide();
 		$("#findinpage-button").click(this.startsearch);
-		this.editor.mousedown(this.endsearch);
+		this.findinpageinput.blur(this.endsearch);
 		this.findinpageinput.keyup(this.highlightmatches);
-		this.findinpageinput.keyup(this.showinfo);
-		this.editor.mousedown(this.hideinfo);
 	}
 });
