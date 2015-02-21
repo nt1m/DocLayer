37c5927aa133414b3f5c03c01ec2df84c21388dd
palmerAL <PalmerAL@users.noreply.github.com>
2015-02-21 12:33:59 -0600

    fix research pane scroll issue

--- js/research.js
+++ js/research.js
@@ -29,7 +29,9 @@ scratchpad.modules.define("research", {
 	imageInsertFlow: function(e) {
 		var _ = this;
 		var position = $(e.target).offset();
-		$(".research-insert-button").css({left: position.left, top: position.top});
+		var shellposition = $(".infocard-shell").offset();
+		var scroll = $(".infocard-shell").scrollTop();
+		$(".research-insert-button").css({left: position.left - shellposition.left, top: position.top - shellposition.top + scroll });
 		$(".research-insert-button").show();
 		$(".research-insert-button").off();
 		$(".research-insert-button").on("mousedown", function() {
@@ -39,7 +41,7 @@ scratchpad.modules.define("research", {
 	init: function() {
 		var _ = this;
 		this.imageInsertFlow = this.imageInsertFlow.bind(this);
-		$(document.body).append('<div noprint hidden class="research-insert-button small fab color-green-500" title="Add image to document"><i class="icon-add"></i></div>');
+		$(".infocard-shell").append('<div noprint hidden class="research-insert-button small fab color-green-500" title="Add image to document"><i class="icon-add"></i></div>');
 		$(".infocard-shell").on("mouseover", "img", function(e) {
 			_.imageInsertFlow(e);
 		});
