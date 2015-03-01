1671861aeb53c1e53128d4cf6a55c4e04b98e6d0
palmerAL <PalmerAL@users.noreply.github.com>
2015-03-01 14:03:50 -0600

    update material-framework

--- editor/lib/material-framework/demo-files/demo.js
+++ editor/lib/material-framework/demo-files/demo.js
@@ -1,9 +1,11 @@
+"use strict";
 function init() {
-	var sm = document.querySelector("#navigation-sidemenu");
-	var smitems = sm.querySelectorAll(".menu > li:not(.divider) > a");
-	for (var i = 0; i < smitems.length; i++) {
-		smitems[i].addEventListener("click", function() {
-			if (document.body.clientWidth < 800) {
+	var sm = document.getElementById("navigation-sidemenu"),
+	    smitems = sm.querySelectorAll(".menu > li:not(.divider) > a");
+
+	function clickHandler() {
+		return function() {
+			if (Responsive.device != "desktop") {
 				SideMenu.hide(sm);
 			}
 			for (var ind = 0; ind < smitems.length; ind++) {
@@ -11,11 +13,17 @@ function init() {
 			}
 			this.parentNode.className = "selected color-blue-500";
 			document.querySelector(".main-content").scrollTop = 0;
-		});
+		};
+	}
+
+	for (var i = 0, len = smitems.length; i < len; i++) {
+		smitems[i].addEventListener("click", clickHandler());
 	}
 }
+
 window.addEventListener("DOMContentLoaded", function() {
-	if ((window.location.hash == "") || (document.querySelector(".navigation-section" + window.location.hash) == null)) {
+	var md = new Material();
+	if ((window.location.hash === "") || (document.querySelector(".navigation-section" + window.location.hash) === null)) {
 		window.location.hash = "#introduction";
 	}
 	document.querySelector("#navigation-sidemenu a[href='" + window.location.hash + "']").parentNode.className = "selected color-blue-500";
