89bae991a03582ba0ab73a47211f0f621f0b6f21
palmerAL <PalmerAL@users.noreply.github.com>
2015-03-20 16:34:28 -0500

    fix #134

--- editor/js/caret.js
+++ editor/js/caret.js
@@ -1,7 +1,13 @@
 //caret pasting api
 
 scratchpad.modules.define("caret", {
+	editor: $("#document-editor"),
 	pasteHtmlAtCaret: function(html, selectPastedContent) {
-			document.execCommand("insertHTML", false, html); //TODO add fallback if this is unsupported by using the old caret api
+		 	var origHTML = this.editor.html();
+			document.execCommand("insertHTML", false, html);
+			//if the caret isn't in the editor, we want to append the html instead. however, there are cases where activeElement is not the editor even though the caret is in the editor, so instead we check if the html was changed by the execCommand and if not append the data instead.
+			if(origHTML == this.editor.html()) {
+				this.editor.append(html);
+			}
 	}
 });
