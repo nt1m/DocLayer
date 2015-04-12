2574e134c9c44bd12733beb8cd5e77ce032bd361
palmerAL <PalmerAL@users.noreply.github.com>
2015-04-12 15:22:25 -0500

    places search for maps

--- editor/js/maps.js
+++ editor/js/maps.js
@@ -1,66 +1,197 @@
 scratchpad.modules.define("maps", {
 	html: '\
 		<div noprint class="dialog map-dialog" hidden>\
-		<span class="dialog-title">Choose map coordinates</span>\
+		<span class="dialog-title">Choose map location</span>\
 		<div class="dialog-content">\
-			<input type="text" class="map-lat-chooser text-input" placeholder="Latitude"/>\
-			<input type="text" class="map-long-chooser text-input" placeholder="Longitude"/>\
+			<input type="search" id="map-search" class="text-input" placeholder="Enter an address"/>\
+			<ul id="map-results" class="list">\
+			</ul>\
 		</div>\
 		<div class="dialog-footer">\
 			<span class="float-right">\
 				<button id="map-cancel-button" class="button dialog-cancel color-accent-color">Cancel</button>\
-				<button id="map-okay-button" class="button dialog-confirm color-accent-color">Add map</button>\
 			</span>\
 		</div>\
 	</div>\
 	',
-	ondialogopen: function() {
-			var input = "<iframe sandbox='allow-scripts' class='mapplaceholder'/>"; //add a placeholder to mark the cursor position
-				scratchpad.caret.pasteHtmlAtCaret(input, false);
+	geolocation_options: {
+		enableHighAccuracy: false,
+		maximumAge: 60000,
+		timeout: 10000
 	},
-	ondialogcancel: function() {
+	ondialogopen: function () {
+		var input = "<iframe sandbox='allow-scripts' class='mapplaceholder'/>"; //add a placeholder to mark the cursor position
+		scratchpad.caret.pasteHtmlAtCaret(input, false);
+	},
+	ondialogcancel: function () {
 		$(".mapplaceholder").remove();
 	},
-	parseCoordinateToInteger: function(point) { //turns any standard latitude/longitude formatted string and turns it into an integer
-		var parsed = point.toUpperCase(); //get any letters in uppercase
-		parsed = parsed.replace("°", ""); //strip out the degrees symbol
-		parsed = parsed.replace(/\s/g, ""); //remove whitespace
-		if(parsed.indexOf("S") > -1 || parsed.indexOf("W") > -1) { //these will be a negative number
-			var is_negative_number = true;
+	getIconFromType: function (type) {
+		var icon;
+		switch (type) {
+		case "bus_stop":
+		case "bus_station":
+			icon = "icon-directions-bus";
+			break;
+		case "subway":
+		case "tram_stop":
+			icon = "icon-directions-subway";
+			break;
+		case "ferry_terminal":
+			icon = "icon-directions-ferry";
+			break;
+		case "pharmacy":
+			icon = "icon-pharmacy";
+			break;
+		case "aerodrome":
+			icon = "icon-airport";
+			break;
+		case "car":
+			icon = "icon-directions-car";
+			break;
+		case "parking":
+			icon = "icon-parking";
+			break;
+		case "station":
+		case "railway":
+			icon = "icon-directions-train";
+			break;
+		case "bank":
+			icon = "icon-account-balance";
+			break;
+		case "bakery":
+			icon = "icon-cake";
+			break;
+		case "bar":
+			icon = "icon-bar";
+			break;
+		case "deli":
+		case "restaurant":
+		case "supermarket":
+		case "grocery":
+		case "greengrocer":
+			icon = "icon-restaurant";
+			break;
+		case "cafe":
+			icon = "icon-cafe";
+			break;
+		case "library":
+		case "books":
+			icon = "icon-library";
+			break;
+		case "retail":
+		case "mall":
+		case "marketplace":
+		case "shop":
+		case "clothes": //TODO find a better icon for this
+			icon = "icon-offer";
+			break;
+		case "park":
+		case "nature_reserve":
+			icon = "icon-landscape";
+			break;
+		case "florist":
+			icon = "icon-florist";
+			break;
+		case "cinema":
+			icon = "icon-movie";
+			break;
+		case "theatre":
+		case "musical_instrument":
+			icon = "icon-audiotrack";
+			break;
+		case "fuel":
+			icon = "icon-gas-station";
+			break;
+		case "computer":
+			icon = "icon-laptop";
+			break;
+		default:
+			icon = "icon-place";
+			break;
 		}
-		parsed = parsed.replace(/[A-Z]/g, ""); //strip out any letters
-		parsed = parseFloat(parsed); //we might need to multiply this
-		if(is_negative_number) {
-			parsed = parsed * -1;
-		}
-		return parsed;
+		return icon;
 	},
-	insertmapfromdialog: function() {
-		var lat = this.parseCoordinateToInteger($(".map-lat-chooser").val());
-		var long = this.parseCoordinateToInteger($(".map-long-chooser").val());
-		
+	insertMap: function (lat, lon) {
+
 		var placeholder = $(".mapplaceholder");
-		placeholder.attr("src", "extend-maps/map.html#" + lat + "," + long);
+		placeholder.attr("src", "extend-maps/map.html#" + lat + "," + lon);
 		placeholder.removeClass("mapplaceholder").addClass("extend-block").addClass("map-extend-block"); //use the placeholder to add a map
 		scratchpad.ui.dialogs.hide(this.dialogEl);
 	},
-	init: function() {
+	setToCurrentLocation: function () {
 		var _ = this;
-		this.insertmapfromdialog = this.insertmapfromdialog.bind(this);
+		navigator.geolocation.getCurrentPosition(function (position) {
+			_.insertMap(position.coords.latitude, position.coords.longitude);
+		}, null, this.geolocation_options);
+	},
+	updateResults: function (searchterm) { //shows the results
+		var _ = this;
+		$.ajax("https://open.mapquestapi.com/nominatim/v1/search.php?format=json&limit=10&q=" + encodeURIComponent(searchterm))
+			.done(function (results) {
+				_.resultsList.html(""); //clear any previous results
+				results.forEach(function (r) {
+					var name_address_array = r.display_name.replace(", ", "///").split("///"); //the address and name are mixed together seperated by a comma, split them up
+
+					//extract the item type
+					var itemtype = r.type;
+					if (itemtype == "yes") { //sometimes, the class contains the actual info and the type is just "yes"
+						itemtype = r.class;
+					}
+
+					//add the item to the list
+					var item = $("<li ripple>");
+					$("<i class='item-action'>").addClass(_.getIconFromType(itemtype)).attr("title", itemtype).appendTo(item); //add the icon
+					var title = $("<span class='item-text'>");
+					title.text(name_address_array[0]);
+					var addressbox = $("<span class='secondary-text'>");
+					addressbox.text(name_address_array[1]);
+					addressbox.appendTo(title);
+					title.appendTo(item);
+					item.appendTo(_.resultsList);
+
+					//click event
+					item.on("click", function () {
+						_.insertMap(r.lat, r.lon);
+					});
+				});
+				if (results.length == 0) {
+					_.resultsList.append("<span class='secondary-text'>No results found.</span")
+				}
+			})
+			.fail(function () {
+				_.resultsList.append("<span class='secondary-text'>Error finding places.</span")
+			})
+	},
+	init: function () {
+		var _ = this;
+		this.insertMap = this.insertMap.bind(this);
+		this.setToCurrentLocation = this.setToCurrentLocation.bind(this);
+		this.updateResults = this.updateResults.bind(this);
 		this.dialogEl = $(".map-dialog");
-		
+		this.resultsList = $("#map-results");
+
 		scratchpad.menu.addItem({
 			color: "white",
 			background: "green-500",
 			name: "map",
 			icon: "icon-map",
-			fn: function() {
+			fn: function () {
 				scratchpad.ui.dialogs.show(_.dialogEl);
 			}
 		});
-		
+
 		this.dialogEl.on("dialog-shown", this.ondialogopen);
 		this.dialogEl.on("dialog-cancel", this.ondialogcancel);
-		this.dialogEl.on("dialog-confirm", this.insertmapfromdialog)
+
+		if ("geolocation" in navigator) {
+			this.dialogEl.find(".dialog-footer").prepend('<button id="map-geolocation-button" class="button color-accent-color">Use my location</button>');
+		}
+		$("#map-geolocation-button").on("click", function () {
+			_.setToCurrentLocation();
+		});
+		$("#map-search").on("change", function () {
+			_.updateResults($(this).val());
+		});
 	}
 });
