26fe7656b1b45c4078393943506b3164da2c411e
palmerAL <PalmerAL@users.noreply.github.com>
2015-04-12 15:20:55 -0500

    allow importing from google docs/word

---
+++ editor/js/contentimport.js
@@ -0,0 +1,43 @@
+/* convert pasted text from google docs/word */
+
+scratchpad.modules.define("contentimport", {
+	css: false,
+	import: function (e) {
+		var data = e.originalEvent.clipboardData;
+		var content = data.getData('text/html');
+		if (!content) { //there is no html data, try to get as plaintext instead
+			content = data.getData('text/plain');
+		}
+		var boxset = $("<div>" + content + "</div>");
+		boxset.find("span").each(function () { //remove google docs formatting spans
+			var $this = $(this);
+			var style = $this.attr("style");
+			var contents = $this.contents();
+			contents.unwrap(); //remove the old span
+			if (style.indexOf("font-weight:bold") > -1) {
+				contents.wrap("<b>");
+			}
+			if (style.indexOf("font-style:italic") > -1) {
+				contents.wrap("<i>");
+			}
+			if (style.indexOf("text-decoration:underline") > -1) {
+				contents.wrap("<u>");
+			}
+		});
+		boxset.find("*").removeAttr("style").removeAttr("id").removeAttr("class"); //remove gogle docs inline styles and docs-internal-guid's
+		boxset.find("img").addClass("extend-block").addClass("img-extend-block").removeAttr("width").removeAttr("height");
+		boxset.find("table").remove(); //these aren't supported correctly
+		boxset.find("meta").remove(); //these are useless
+		boxset.find("style").remove(); //these are useless
+		boxset.find("link").remove(); //these are useless
+		var htmlstring = boxset[0].innerHTML;
+		document.execCommand("insertHTML", false, htmlstring);
+	},
+	init: function () {
+		var _ = this;
+		$("#document-editor").on("paste", function (e) {
+			e.preventDefault();
+			_.import(e);
+		});
+	}
+});
--- editor/js/main.js
+++ editor/js/main.js
@@ -1,38 +1,38 @@
 var scratchpad = {
 	modules: {
-		load: function(name) {
+		load: function (name) {
 			var scriptEl = $("<script async>").attr("src", "js/" + name + ".js");
 			scriptEl.appendTo("body");
-			scriptEl.on("error", function() {
+			scriptEl.on("error", function () {
 				console.log("[scratchpad.modules.load] Failed to load module: " + name);
 			});
 		},
-		define: function(name, code) {
+		define: function (name, code) {
 			scratchpad[name] = code;
-			if(scratchpad[name].hasOwnProperty("html")) {
+			if (scratchpad[name].hasOwnProperty("html")) {
 				$(document.body).append(scratchpad[name].html);
 			}
-			if(scratchpad[name].css != false) { //this is loaded at define because it needs to access the css property of the module
+			if (scratchpad[name].css != false) { //this is loaded at define because it needs to access the css property of the module
 				var stylesheet = $("<link>").attr("rel", "stylesheet").attr("href", "css/" + name + ".css");
 				stylesheet.appendTo("body");
 			}
-			if(scratchpad[name].hasOwnProperty("init")) {
+			if (scratchpad[name].hasOwnProperty("init")) {
 				scratchpad[name].init();
 			}
 		}
 	},
-	init: function() {
+	init: function () {
 		var starttime = Date.now(); // start performance logging
 
 		//DO NOT remove core moduels
 		var core = ["keybindings", "ui", "caret", "editortooltip", "darktheme"]; //the darktheme is core to reduce the white flash before loading
-		core.forEach(function(value) {
+		core.forEach(function (value) {
 			scratchpad.modules.load(value);
 		});
-		var modulesToLoad = ["menu", "editor", "images", "maps", "videos", "embeds", "findinpage", "research", "charts", "comments", "imageEditor", "spellcheck" ];
-		$(window).load(function() {
+		var modulesToLoad = ["menu", "editor", "images", "maps", "videos", "embeds", "findinpage", "research", "charts", "comments", "imageEditor", "spellcheck", "contentimport"];
+		$(window).load(function () {
 			$(".splashscreen.loading").hide();
-			modulesToLoad.forEach(function(value) {
+			modulesToLoad.forEach(function (value) {
 				scratchpad.modules.load(value);
 			});
 		});
@@ -42,6 +42,6 @@ var scratchpad = {
 		console.log("modules loaded in " + diff + " ms");
 	}
 }
-$(function() {
+$(function () {
 	scratchpad.init();
 });
