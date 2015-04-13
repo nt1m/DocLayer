a971de470d5d3dda261827174692917aacff4294
palmerAL <PalmerAL@users.noreply.github.com>
2015-04-13 16:47:36 -0500

    autofocus the document editor

--- editor/js/editor.js
+++ editor/js/editor.js
@@ -1,39 +1,43 @@
 scratchpad.modules.define("editor", {
-	insertItem: function(item) {
-		switch(item) {
-			case "divider":
-				var input = "<hr class='divider'></hr>";
-				break;
+	insertItem: function (item) {
+		switch (item) {
+		case "divider":
+			var input = "<hr class='divider'></hr>";
+			break;
 		}
 		scratchpad.caret.pasteHtmlAtCaret(input, false);
 	},
-	showDeletion: function(item) {
+	showDeletion: function (item) {
 		var button = this.deletionButton;
 		var offset = item.offset();
 		var itemwidth = item.width();
 
-		button.css({top: offset.top, left: offset.left});
+		button.css({
+			top: offset.top,
+			left: offset.left
+		});
 		button.show();
 		button.off();
-		button.on("click", function() {
+		button.on("click", function () {
 			item.remove();
 			button.hide();
 		});
 	},
-	hideDeletion: function() {
+	hideDeletion: function () {
 		this.deletionButton.hide();
 	},
-	starItem: function(e) {
+	starItem: function (e) {
 		var target = $(e.target);
-		if(e.pageX < target.offset().left) {
-			if(target.attr("starred")) {
+		if (e.pageX < target.offset().left) {
+			if (target.attr("starred")) {
 				target.removeAttr("starred");
 			} else {
 				target.attr("starred", "true");
 			}
 		}
 	},
-	init: function() {
+	init: function () {
+		$("#document-editor").get(0).focus(); //autofocus the editor
 		var _ = this;
 		$(document.body).append('<div noprint class="deletion-button small fab color-red-500" title="Remove"><i class="icon-delete"></i></div>'); //add the deletion button
 		this.deletionButton = $(".deletion-button");
@@ -43,21 +47,21 @@ scratchpad.modules.define("editor", {
 			background: "white",
 			name: "divider",
 			icon: "icon-border-horizontal",
-			fn: function() {
-			_.insertItem("divider");
+			fn: function () {
+				_.insertItem("divider");
 			}
 		});
 
-		$("#document-editor").on( "mouseover", ".extend-block", function() {
+		$("#document-editor").on("mouseover", ".extend-block", function () {
 			_.showDeletion($(this));
 		});
-		$("#document-editor").on("click", function() {
+		$("#document-editor").on("click", function () {
 			_.hideDeletion();
 		});
-		$("#document-editor").on("click", function(e) {
+		$("#document-editor").on("click", function (e) {
 			_.starItem(e);
 		});
-		scratchpad.keybindings.addBinding("tab", function() {
+		scratchpad.keybindings.addBinding("tab", function () {
 			document.execCommand("insertHTML", false, "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"); //TODO make this use real tabs (would require wrapping the editor in a <pre> block)
 		});
 	}
