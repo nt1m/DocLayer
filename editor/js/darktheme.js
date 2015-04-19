70a4e6e90fffc532f49e3ae1b70733d5eaeb01d3
palmerAL <PalmerAL@users.noreply.github.com>
2015-04-19 14:39:33 -0500

    don't load unnecessary files

--- editor/js/darktheme.js
+++ editor/js/darktheme.js
@@ -1,30 +1,27 @@
 scratchpad.modules.define("darktheme", {
-	checktheme: function() {
+	css: false,
+	checktheme: function () {
 		var d = new Date();
 
 		var curr_hour = d.getHours();
 
-		if(curr_hour < 6 || curr_hour > 20) {
+		if (curr_hour < 6 || curr_hour > 20) {
 			$(document.body).addClass("dark-theme");
-		}
-		else {
+		} else {
 			$(document.body).removeClass("dark-theme");
 		}
 
 	},
-	disableThemeSwitching: function() {
+	disableThemeSwitching: function () {
 		clearInterval(this.interval);
 		this.interval = null;
 	},
-	enableThemeSwitching: function() {
+	enableThemeSwitching: function () {
 		this.interval = setInterval(this.checktheme, 30000);
 	},
-	init: function() {
+	init: function () {
 		var _ = this;
 		this.checktheme();
 		this.interval = setInterval(this.checktheme, 30000);
 	}
 });
-
-
-
