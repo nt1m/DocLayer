b1fc260635128c43d9a0c76576faa269020c5731
palmerAL <PalmerAL@users.noreply.github.com>
2015-05-22 11:32:36 -0500

    theme switching backend

--- editor/js/theme.js
+++ editor/js/theme.js
@@ -6,11 +6,23 @@ var theme_module = {
 		var curr_hour = d.getHours();
 
 		if (curr_hour < 6 || curr_hour > 20) {
+			this.switchToTheme("dark");
+		} else if (this.selectedTheme && this.selectedTheme != "dark") {
+			this.switchToTheme(this.selectedTheme);
+		}
+
+	},
+	switchToTheme: function (themeName) {
+		this.selectedTheme = themeName;
+		$("#themestyle").remove();
+		if (themeName == "default") {
+			$(document.body).removeClass("dark-theme");
+		} else if (themeName == "dark") {
 			$(document.body).addClass("dark-theme");
 		} else {
 			$(document.body).removeClass("dark-theme");
+			$("<link rel='stylesheet' id='themestyle'>").attr("href", "../global/themes/" + themeName + ".css").appendTo(document.body);
 		}
-
 	},
 	disableThemeSwitching: function () {
 		clearInterval(this.interval);
@@ -20,12 +32,22 @@ var theme_module = {
 		this.interval = setInterval(this.checktheme, 30000);
 	},
 	init: function () {
+		var _ = this;
+		this.checktheme = this.checktheme.bind(this);
+		this.switchToTheme = this.switchToTheme.bind(this);
 		this.checktheme();
-		this.interval = setInterval(this.checktheme, 30000);
+		this.enableThemeSwitching();
+
+		getPref("theme", function (theme) {
+			if (theme) { //the pref has a value
+				_.switchToTheme(theme);
+				_.selectedTheme = theme;
+			}
+		});
 	}
 }
 
-if(window.scratchpad && window.scratchpad.modules) {
+if (window.scratchpad && window.scratchpad.modules) {
 	scratchpad.modules.define("theme", theme_module);
 } else {
 	window.theme = theme_module;
