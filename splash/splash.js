52eabe73093b63bccd99cdcd7751fe1aec8c2404
palmerAL <PalmerAL@users.noreply.github.com>
2015-06-02 17:24:28 -0500

    (signin page) performance improvements

--- splash/splash.js
+++ splash/splash.js
@@ -14,14 +14,18 @@ client.authenticate({
 	if (client.isAuthenticated()) {
 		window.location = config.basepath + "doclist";
 	} else {
-		$(".signin-button").on("click", function () {
-			// The user will have to click an 'Authorize' button.
-			client.authenticate(function (error, client) {
-				if (error) {
-					return handleError(error);
-				}
-				window.location = config.basepath + "doclist";
+
+		$(document).ready(function () {
+			$(".signin-button").on("click", function () {
+				// The user will have to click an 'Authorize' button.
+				client.authenticate(function (error, client) {
+					if (error) {
+						return handleError(error);
+					}
+					window.location = config.basepath + "doclist";
+				});
 			});
 		});
+
 	}
 });
