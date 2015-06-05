a3c1e2f585ee9aad5880fd726531210c22db2fbc
palmerAL <PalmerAL@users.noreply.github.com>
2015-06-05 15:55:16 -0500

    fix some bugs with theme switching

--- global/theme.js
+++ global/theme.js
@@ -1,27 +1,36 @@
 scratchpad.modules.define("theme", {
 	css: false,
 	checktheme: function () {
+		var _ = this;
 		var d = new Date();
 
 		var curr_hour = d.getHours();
 
 		if (curr_hour < 6 || curr_hour > 20) {
 			this.switchToTheme("dark");
-		} else if (this.selectedTheme && this.selectedTheme != "dark") {
-			this.switchToTheme(this.selectedTheme);
+		} else {
+			getPref("theme", function (theme) {
+				_.switchToTheme(theme);
+			});
 		}
 
 	},
 	switchToTheme: function (themeName) {
 		this.selectedTheme = themeName;
-		$("#themestyle").remove();
 		if (themeName == "default") {
 			$(document.body).removeClass("dark-theme");
+			$("#themestyle").remove();
 		} else if (themeName == "dark") {
 			$(document.body).addClass("dark-theme");
+			$("#themestyle").remove();
 		} else {
 			$(document.body).removeClass("dark-theme");
-			$("<link rel='stylesheet' id='themestyle'>").attr("href", "../global/themes/" + themeName + ".css").appendTo(document.body);
+			var previousThemeSource = $("#themestyle").attr("href");
+			var newThemeSource = "../global/themes/" + themeName + ".css";
+			if (previousThemeSource != newThemeSource) {
+				$("#themestyle").remove();
+				$("<link rel='stylesheet' id='themestyle'>").attr("href", "../global/themes/" + themeName + ".css").appendTo(document.body);
+			}
 		}
 	},
 	disableThemeSwitching: function () {
@@ -29,6 +38,7 @@ scratchpad.modules.define("theme", {
 		this.interval = null;
 	},
 	enableThemeSwitching: function () {
+		this.checktheme();
 		this.interval = setInterval(this.checktheme, 30000);
 	},
 	init: function () {
