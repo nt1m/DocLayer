c61fcf7c85705b34babba744abc67968100a1ec6
palmerAL <PalmerAL@users.noreply.github.com>
2015-06-10 17:17:57 -0500

    add satellite imagery support in maps

--- editor/extend-maps/maps.js
+++ editor/extend-maps/maps.js
@@ -5,11 +5,42 @@ var coords = hash.split(",");
 var lat = coords[0]
 var lon = coords[1]
 
+var tabs = {
+	satellite: $("[mode=satellite]"),
+	map: $("[mode=regular]"),
+}
+
 var map = L.map('map-main').setView([lat, lon], 13);
-L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
+var mapLayer = L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
 	maxZoom: 18,
 	subdomains: '1234',
-	attribution: "Data, imagery and map information provided by MapQuest, <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> and contributors, <a href='http://wiki.openstreetmap.org/wiki/Legal_FAQ#3a._I_would_like_to_use_OpenStreetMap_maps._How_should_I_credit_you.3F'>ODbL </a>"
-}).addTo(map);
+	attribution: 'Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a>. &copy; OpenStreetMap contributors (<a href="http://www.openstreetmap.org/copyright" target="_blank">license</a>)'
+});
+
+mapLayer.addTo(map);
 
 var marker = L.marker([lat, lon]).addTo(map);
+
+tabs.map.on("click", function () {
+	if (satelliteLayer) {
+		map.addLayer(mapLayer);
+		map.removeLayer(satelliteLayer);
+	}
+	tabs.satellite.removeClass("selected");
+	tabs.map.addClass("selected");
+});
+
+tabs.satellite.on("click", function () {
+	if (!window.satelliteLayer) { //there isn't a satellite layer yet, create one
+		window.satelliteLayer = L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png', {
+			maxZoom: 18,
+			subdomains: '1234',
+			attribution: 'Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a>. Portions Courtesy NASA/JPL-Caltech and U.S. Depart. of Agriculture, Farm Service Agency'
+		});
+	}
+
+	map.addLayer(satelliteLayer);
+	map.removeLayer(mapLayer);
+	tabs.satellite.addClass("selected");
+	tabs.map.removeClass("selected");
+})
