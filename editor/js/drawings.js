d83d60a1a49b6ab1ccba2705fd01fc9f9c55dfb1
PalmerAL <PalmerAL@users.noreply.github.com>
2015-08-13 11:32:31 -0500

    make the eraser wider in the drawing editor
    #203

--- editor/js/drawings.js
+++ editor/js/drawings.js
@@ -15,6 +15,10 @@ scratchpad.modules.define("drawings", {
 		</div>\
 	</div>\
 	',
+	penWidths: {
+		minWidth: 0.75,
+		maxWidth: 2,
+	},
 	colors: ["#000000", "#ffffff", "#f44336", "#e91e63", "#9c27b0", "#2196f3", "#4caf50", "#ff5722", "#607d8b"],
 	ondialogopen: function () {
 		//resize canvas to window
@@ -94,10 +98,7 @@ scratchpad.modules.define("drawings", {
 		this.dialogEl.on("dialog-confirm", this.insertdrawingfromdialog);
 
 		this.canvas = $("#drawing-editor-canvas");
-		this.signaturePad = new SignaturePad(this.canvas[0], {
-			minWidth: 0.7,
-			maxWidth: 2,
-		});
+		this.signaturePad = new SignaturePad(this.canvas[0], this.penWidths);
 
 		$(document.body).append('<div noprint class="drawing-edit-button edit-button small fab color-green-500" title="Edit"><i class="icon-create"></i></div>'); //add the edit button
 		this.editButton = $(".drawing-edit-button");
@@ -107,7 +108,15 @@ scratchpad.modules.define("drawings", {
 
 		this.colors.forEach(function (color) {
 			$("<div ripple>").css("background-color", color).appendTo(_.colorPicker).on("click", function () {
-				_.signaturePad.penColor = $(this).css("background-color");
+				var chosenColor = $(this).css("background-color");
+				if (chosenColor == "rgb(255, 255, 255)") { //white should have a larger pen width than other colors, since it functions as an eraser
+					_.signaturePad.minWidth = _.penWidths.minWidth * 3.2;
+					_.signaturePad.maxWidth = _.penWidths.maxWidth * 3.8;
+				} else {
+					_.signaturePad.minWidth = _.penWidths.minWidth;
+					_.signaturePad.maxWidth = _.penWidths.maxWidth;
+				}
+				_.signaturePad.penColor = chosenColor
 			});
 		});
 	}
